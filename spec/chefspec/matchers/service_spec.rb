2b3e8420d022a2b97f21375ea4c88eb5b83ae3a9
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2012-10-04 11:52:59 +0530

    Add support for testing that a service is not running on boot

--- spec/chefspec/matchers/service_spec.rb
+++ spec/chefspec/matchers/service_spec.rb
@@ -24,6 +24,28 @@ module ChefSpec
           matcher.matches?({:resources => [{:resource_name => 'service', :service_name => 'food', :action => [:stop, :enable, :start, :restart]}]}).should be true
         end
       end
+
+      describe "#set_service_to_not_start_on_boot" do
+        let(:matcher) { matcher = set_service_to_not_start_on_boot('food') }
+        it "should not match when no resources exist" do
+          matcher.matches?({:resources => []}).should be false
+        end
+        it "should not match if no service resource is declared" do
+          matcher.matches?({:resources => [{:resource_name => 'log', :name => 'Installing service...'}]}).should be false
+        end
+        it "should not match if a different service is declared" do
+          matcher.matches?({:resources => [{:resource_name => 'service', :service_name => 'sendmail', :action => [:stop, :disable]}]}).should be false
+        end
+        it "should not match if the service is set to start but not enabled" do
+          matcher.matches?({:resources => [{:resource_name => 'service', :service_name => 'food', :action => [:start]}]}).should be false
+        end
+        it "should match if the service is disabled" do
+          matcher.matches?({:resources => [{:resource_name => 'service', :service_name => 'food', :action => [:disable]}]}).should be true
+        end
+        it "should match if the service is disabled amongst other actions" do
+          matcher.matches?({:resources => [{:resource_name => 'service', :service_name => 'food', :action => [:stop, :disable, :start, :restart]}]}).should be true
+        end
+      end
     end
   end
 end
