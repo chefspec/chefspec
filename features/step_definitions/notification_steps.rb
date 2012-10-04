63950b46da6f76dce1163099abbb5255975ef3f9
Ranjib Dey <ranjibd@thoughtworks.com>
2012-10-04 13:25:23 +0530

    Adding cuckes for resource notification assert where notified resource has braces in its name

--- features/step_definitions/notification_steps.rb
+++ features/step_definitions/notification_steps.rb
@@ -9,3 +9,9 @@ end
 Then /the notify assertion will be succesfully evaluated/ do
 end
 
+Given /^a Chef cookbook with a recipe in which a template notifies a service having braces in its name$/ do
+  recipe_with_template_notifying_service_having_braces_in_name
+end
+Given /^the recipe has a spec example that assert on the notification service having braces in its name$/ do
+  spec_expects_template_notifies_service_having_braces_in_its_name
+end
