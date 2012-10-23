6494265381d4f087569399aa522c518b8057d9db
Jim Hopp <jim.hopp@mylookout.com>
2012-10-23 12:16:55 -0700

    Add support for chef_gem resource

--- lib/chefspec/matchers/package.rb
+++ lib/chefspec/matchers/package.rb
@@ -3,7 +3,7 @@ require 'chefspec/matchers/shared'
 module ChefSpec
   module Matchers
 
-    define_resource_matchers([:install, :remove, :upgrade, :purge], [:package, :gem_package], :package_name)
+    define_resource_matchers([:install, :remove, :upgrade, :purge], [:package, :gem_package, :chef_gem], :package_name)
 
     RSpec::Matchers.define :install_package_at_version do |package_name, version|
       match do |chef_run|
@@ -16,6 +16,13 @@ module ChefSpec
       match do |chef_run|
        chef_run.resources.any? do |resource|
           resource_type(resource) == 'gem_package' and resource.package_name == package_name and resource.action.to_s.include? 'install' and resource.version == version
+        end
+      end
+    end
+    RSpec::Matchers.define :install_chef_gem_at_version do |package_name, version|
+      match do |chef_run|
+       chef_run.resources.any? do |resource|
+          resource_type(resource) == 'chef_gem' and resource.package_name == package_name and resource.action.to_s.include? 'install' and resource.version == version
         end
       end
     end
---
+++ spec/chefspec/matchers/chef_gem_package_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module ChefSpec
+  module Matchers
+    describe :install_chef_gem_at_version do
+      describe "#match" do
+        let(:matcher) { install_chef_gem_at_version('foo', '1.2.3') }
+        it "should not match when no resources exist" do
+          matcher.matches?({:resources => []}).should be false
+        end
+        it "should not match when there are no chef gem packages" do
+          matcher.matches?({:resources => [{:resource_name => 'template', :path => '/tmp/config.conf',
+            :source => 'config.conf.erb'}]}).should be false
+        end
+        it "should not match if is a different chef gem package and an unspecified version" do
+          matcher.matches?({:resources => [{:resource_name => 'chef_gem', :package_name => 'bar', :version => nil, :action => :install}]}).should be false
+        end
+        it "should not match if it is the same chef gem package and version but a different action" do
+          matcher.matches?({:resources => [{:resource_name => 'chef_gem', :package_name => 'foo', :version => '1.2.3', :action => :upgrade}]}).should be false
+        end
+        it "should match if is the same chef gem package, the correct version and the install action" do
+          matcher.matches?({:resources => [{:resource_name => 'chef_gem', :package_name => 'foo', :version => '1.2.3', :action => :install}]}).should be true
+        end
+      end
+    end
+  end
+end
