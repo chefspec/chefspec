7d9566a5d7fe194b62c4d10250d81f1a7bab9b17
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-02-11 11:44:38 +0100

    Test all script matchers

--- spec/chefspec/matchers/script_spec.rb
+++ spec/chefspec/matchers/script_spec.rb
@@ -3,37 +3,42 @@ require 'spec_helper'
 module ChefSpec
   module Matchers
     describe :script do
-      describe '#match' do
-        let(:matcher) { execute_bash_script('foo') }
+      %w(bash csh perl python ruby).each do |interpreter|
+        describe interpreter do
+          let(:matcher) do
+            matcher_name = "execute_#{interpreter}_script"
+            send(matcher_name, 'foo')
+          end
 
-        it 'should not match when no resources exist' do
-          matcher.matches?(:resources => []).should be false
-        end
+          it "should not match when no resources exist" do
+            matcher.matches?(:resources => []).should be false
+          end
 
-        it 'should match when bash resource exists' do
-          resources = [{:resource_name => 'bash', :name => 'foo'}]
-          matcher.matches?(:resources => resources).should be true
-        end
+          it "should match when #{interpreter} resource exists" do
+            resources = [{:resource_name => interpreter, :name => 'foo'}]
+            matcher.matches?(:resources => resources).should be true
+          end
 
-        it 'should match when script resource with bash interpreter exists' do
-          resources = [{:resource_name => 'script', :interpreter => 'bash', :name => 'foo'}]
-          matcher.matches?(:resources => resources).should be true
-        end
+          it "should match when script resource with #{interpreter} interpreter exists" do
+            resources = [{:resource_name => 'script', :interpreter => interpreter, :name => 'foo'}]
+            matcher.matches?(:resources => resources).should be true
+          end
 
-        it 'should match when there is another bash resource' do
-          resources = [{:resource_name => 'bash', :name => 'foo'},
-                       {:resource_name => 'bash', :name => 'bar'}]
-          matcher.matches?(:resources => resources).should be true
-        end
+          it "should match when there is another #{interpreter} resource" do
+            resources = [{:resource_name => interpreter, :name => 'foo'},
+                         {:resource_name => interpreter, :name => 'bar'}]
+            matcher.matches?(:resources => resources).should be true
+          end
 
-        it 'should not match when bash resource with another name exists' do
-          resources = [{:resource_name => 'bash', :name => 'bar'}]
-          matcher.matches?(:resources => resources).should be false
-        end
+          it "should not match when #{interpreter} resource with another name exists" do
+            resources = [{:resource_name => 'bash', :name => 'bar'}]
+            matcher.matches?(:resources => resources).should be false
+          end
 
-        it 'should not match when script resource with another interpreter exists' do
-          resources = [{:resource_name => 'script', :interpreter => 'perl', :name => 'foo'}]
-          matcher.matches?(:resources => resources).should be false
+          it "should not match when script resource with another interpreter exists" do
+            resources = [{:resource_name => 'script', :interpreter => 'bar', :name => 'foo'}]
+            matcher.matches?(:resources => resources).should be false
+          end
         end
       end
     end
