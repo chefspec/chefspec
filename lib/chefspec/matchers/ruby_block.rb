edbbb050755d374dc87a09d979f641d4845516d0
Simeon Simeonov <sim@fastignite.com>
2013-06-16 00:06:33 -0400

    extends ruby_block matcher with Regexp expectations

--- lib/chefspec/matchers/ruby_block.rb
+++ lib/chefspec/matchers/ruby_block.rb
@@ -5,7 +5,7 @@ module ChefSpec
     RSpec::Matchers.define :execute_ruby_block do |block_name|
       match do |chef_run|
         chef_run.resources.any? do |resource|
-          resource_type(resource) == 'ruby_block' and resource.name == block_name
+          resource_type(resource) == 'ruby_block' and block_name === resource.name
         end
       end
     end
--- spec/chefspec/matchers/ruby_block_spec.rb
+++ spec/chefspec/matchers/ruby_block_spec.rb
@@ -3,15 +3,32 @@ require 'spec_helper'
 module ChefSpec
   module Matchers
     describe :ruby_block do
-      let(:matcher){execute_ruby_block('sample')}
-      it "shouldn't match when no resources exists" do
-        matcher.matches?({:resources => []}).should be false
+      context 'with String expectations' do
+        let(:matcher) { execute_ruby_block('sample') }
+        it "shouldn't match when no resources exists" do
+          matcher.matches?({:resources => []}).should be false
+        end
+        it "should match when ruby_block resource exists" do
+          matcher.matches?({:resources => [{:resource_name => 'ruby_block', :action => :create, :name => 'sample'}]}).should be true
+        end
+        it "should not match when the ruby_block resource with another name exists" do
+          matcher.matches?({:resources => [{:resource_name => 'ruby_block', :action => :create, :name => 'not_sample'}]}).should be false
+        end
       end
-      it "should match when ruby_block resource exists" do
-        matcher.matches?({:resources => [{:resource_name => 'ruby_block', :action => :create, :name => 'sample'}]}).should be true
-      end
-      it "should not match when the ruby_block resource with another name exists" do
-        matcher.matches?({:resources => [{:resource_name => 'ruby_block', :action => :create, :name => 'not_sample'}]}).should be false
+      context 'with Regexp expectations' do
+        let(:matcher) { execute_ruby_block(/sample/) }
+        it "shouldn't match when no resources exists" do
+          matcher.matches?({:resources => []}).should be false
+        end
+        it "should match when an exact matching ruby_block resource exists" do
+          matcher.matches?({:resources => [{:resource_name => 'ruby_block', :action => :create, :name => 'sample'}]}).should be true
+        end
+        it "should match when a matching ruby_block resource exists" do
+          matcher.matches?({:resources => [{:resource_name => 'ruby_block', :action => :create, :name => 'still_sample'}]}).should be true
+        end
+        it "should not match when the ruby_block resource with a non-matching name exists" do
+          matcher.matches?({:resources => [{:resource_name => 'ruby_block', :action => :create, :name => 'altogether different'}]}).should be false
+        end
       end
     end
   end
