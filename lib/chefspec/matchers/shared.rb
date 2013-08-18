476ce430983b67197c7454ad83721aef71719ba3
Seth Vargo <sethvargo@gmail.com>
2013-08-18 14:57:30 -0400

    Remove legacy code and cleanup features

--- lib/chefspec/matchers/shared.rb
+++ lib/chefspec/matchers/shared.rb
@@ -49,31 +49,36 @@ end
 
 # Compute the contents of a template using Chef's templating logic.
 #
-# @param [Chef::RunContext]
+# @param [Chef::RunContext] chef_run
 #   the run context for the node
-# @param [Chef::Provider::Template]
+# @param [Chef::Provider::Template] template
 #   the template resource
 #
 # @return [String]
 def content_from_template(chef_run, template)
   cookbook_name = template.cookbook || template.cookbook_name
-  template_finder = Chef::Provider::TemplateFinder.new(chef_run.run_context, cookbook_name, chef_run.node)
-  template_location = template_finder.find(template.source)
+  # template_finder = Chef::Provider::TemplateFinder.new(chef_run.run_context, cookbook_name, chef_run.node)
+  template_location = cookbook_collection(chef_run.node)[cookbook_name].preferred_filename_on_disk_location(chef_run.node, :templates, template.source)
 
-  template_context = Chef::Mixin::Template::TemplateContext.new([])
-  template_context.update({
-    :node => chef_run.node,
-    :template_finder => template_finder,
-  }.merge(template.variables))
-
-  template_context.render_template(template_location)
+  if Chef::Mixin::Template.const_defined?(:TemplateContext) # Chef 11+
+    template_context = Chef::Mixin::Template::TemplateContext.new([])
+    template_context.update({
+      :node => chef_run.node,
+      :template_finder => template_finder(chef_run, cookbook_name),
+    }.merge(template.variables))
+    template_context.render_template(template_location)
+  else
+    template.provider.new(template, chef_run.run_context).send(:render_with_context, template_location) do |file|
+      File.read(file.path)
+    end
+  end
 end
 
 # Get the contents of a file resource.
 #
-# @param [Chef::RunContext]
+# @param [Chef::RunContext] chef_run
 #   the run context for the node
-# @param [Chef::Provider::File]
+# @param [Chef::Provider::File] file
 #   the file resource
 #
 # @return [String]
@@ -83,12 +88,43 @@ end
 
 # Get the contents of a cookbook file using Chef.
 #
-# @param [Chef::RunContext]
+# @param [Chef::RunContext] chef_run
 #   the run context for the node
-# @param [Chef::Provider::CookbookFile]
+# @param [Chef::Provider::CookbookFile] cookbook_file
 #   the file resource
 def content_from_cookbook_file(chef_run, cookbook_file)
   cookbook_name = cookbook_file.cookbook || cookbook_file.cookbook_name
   cookbook = chef_run.run_context.cookbook_collection[cookbook_name]
   File.read(cookbook.preferred_filename_on_disk_location(chef_run.node, :files, cookbook_file.source, cookbook_file.path))
+end
+
+# The cookbook collection for the current Chef run context. Handles
+# the differing cases between Chef 10 and Chef 11.
+#
+# @param [Chef::Node] node
+#   the Chef node to get the cookbook collection from
+#
+# @return [Array<Chef::Cookbook>]
+def cookbook_collection(node)
+  if node.respond_to?(:run_context)
+    node.run_context.cookbook_collection # Chef 11+
+  else
+    node.cookbook_collection # Chef 10
+  end
+end
+
+# Return a new instance of the TemplateFinder if we are running on Chef 11.
+#
+# @param [Chef::RunContext] chef_run
+#   the run context for the noe
+# @param [String] cookbook_name
+#   the name of the cookbook
+#
+# @return [Chef::Provider::TemplateFinder, nil]
+def template_finder(chef_run, cookbook_name)
+  if Chef::Provider.const_defined?(:TemplateFinder) # Chef 11+
+    Chef::Provider::TemplateFinder.new(chef_run.run_context, cookbook_name, chef_run.node)
+  else
+    nil
+  end
 end
