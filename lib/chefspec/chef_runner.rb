db54a67ee06f1401d2e6c55baa11436851a3195a
Seth Vargo <sethvargo@gmail.com>
2013-09-11 10:48:07 -0400

    Remove Chef 0.9 references

--- lib/chefspec/chef_runner.rb
+++ lib/chefspec/chef_runner.rb
@@ -113,13 +113,7 @@ module ChefSpec
           end
 
           if runner.step_into.include?(self.resource_name.to_s)
-            # Ignore not_if / only_if guards
-            if self.only_if.is_a?(Array) # 0.10.x
-              self.instance_eval { @not_if = []; @only_if = [] }
-            else # 0.9.x
-              self.only_if { true }
-              self.not_if { false }
-            end
+            self.instance_eval { @not_if = []; @only_if = [] }
             self.old_run_action(action)
           end
 
