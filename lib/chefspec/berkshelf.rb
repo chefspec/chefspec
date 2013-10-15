46495e7a57e1f3f86acfc65512b92944da4e0791
Seth Vargo <sethvargo@gmail.com>
2013-10-15 02:11:59 -0400

    Use RSpec's `before(:suite)` and `after(:suite)` for Berkshelf

--- lib/chefspec/berkshelf.rb
+++ lib/chefspec/berkshelf.rb
@@ -7,38 +7,48 @@ rescue LoadError
     " to your Gemfile:\n\n  gem 'berkshelf'\n\n"
 end
 
-require 'fileutils'
-
 module ChefSpec
   class Berkshelf
     class << self
       extend Forwardable
-      def_delegators :instance, :setup!
+      def_delegators :instance, :setup!, :teardown!
     end
 
     include Singleton
 
     def initialize
+      @tmpdir = Dir.mktmpdir
       setup!
     end
 
+    #
+    # Setup and install the necessary dependencies in the temporary directory.
+    #
     def setup!
-      tmpdir = Dir.mktmpdir
+      teardown! # Berkshelf 3.0 requires the directory to be empty
+      FileUtils.mkdir_p(@tmpdir)
 
       ::Berkshelf.ui.mute do
         if ::Berkshelf::Berksfile.method_defined?(:vendor)
-          FileUtils.rm_rf(tmpdir)
-          ::Berkshelf::Berksfile.from_file('Berksfile').vendor(tmpdir)
+          ::Berkshelf::Berksfile.from_file('Berksfile').vendor(@tmpdir)
         else
-          ::Berkshelf::Berksfile.from_file('Berksfile').install(path: tmpdir)
+          ::Berkshelf::Berksfile.from_file('Berksfile').install(path: @tmpdir)
         end
       end
 
-      ::RSpec.configure do |config|
-        config.cookbook_path = tmpdir
-      end
+      ::RSpec.configure { |config| config.cookbook_path = @tmpdir }
+    end
+
+    #
+    # Destroy the installed Berkshelf at the temporary directory.
+    #
+    def teardown!
+      FileUtils.rm_rf(@tmpdir) if File.exists?(@tmpdir)
     end
   end
 end
 
-ChefSpec::Berkshelf.setup!
+RSpec.configure do |config|
+  config.before(:suite) { ChefSpec::Berkshelf.setup! }
+  config.after(:suite)  { ChefSpec::Berkshelf.teardown! }
+end
