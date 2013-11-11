c071e3044cfe82363194c66eaf7d42766b6b723d
Nathan Zook <nzook@whitepages.com>
2013-11-11 13:28:54 -0800

    Added failing example for issue #258

---
+++ examples/heavy_provider_light_resource/libraries/resource_service.rb
@@ -0,0 +1,15 @@
+require 'chef/resource/service'
+
+class ::Chef
+  class Resource
+    class HeavyProviderLightResourceService < ::Chef::Resource::Service
+      attr_accessor :root
+      def initialize(service_name, run_context=nil)
+        super
+        @resource_name = :mixed_resource
+        @action = :configure
+        @allowed_actions += [:configure]
+      end
+    end
+  end
+end
---
+++ examples/heavy_provider_light_resource/recipes/default.rb
@@ -0,0 +1,4 @@
+heavy_provider_light_resource_service "here" do
+  action 'configure'
+end
+
