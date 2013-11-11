4bfb1cb9524dffd2bc131990ca3e914f4ca521dd
Nathan Zook <nzook@whitepages.com>
2013-11-11 14:44:33 -0800

    Move common code to module

--- lib/chefspec/extensions/chef/lwrp_base.rb
+++ lib/chefspec/extensions/chef/lwrp_base.rb
@@ -1,46 +1,53 @@
 # Override Chef LWRP creation to remove existing class to avoid redefinition warnings.
 class Chef
+  module RemoveExistingLWRP
+    def self.extended(klass)
+      class << klass
+        alias_method :old_build_from_file, :build_from_file
+        remove_method :build_from_file
+      end
+    end
+
+    #
+    # Override Opscode provider to remove any existing LWRPs to suppress
+    # constant re-definition warnings.
+    #
+    # @param [String] cookbook_name
+    #   the name of the cookbook
+    # @param [String] filename
+    #   file to load as a LWRP
+    # @param [Chef::RunContext] run_context
+    #   context of a Chef Run
+    #
+    # @return [Chef::Provider]
+    #
+    def build_from_file(cookbook_name, filename, run_context)
+      provider_name = filename_to_qualified_string(cookbook_name, filename)
+      class_name    = convert_to_class_name(provider_name)
+
+      remove_existing_lwrp(class_name)
+      old_build_from_file(cookbook_name, filename, run_context)
+    end
+
+    #
+    # Remove any existing Chef provider or resource with the specified name.
+    #
+    # @param [String] class_name
+    #   The class name. Must be a valid constant name.
+    #
+    def remove_existing_lwrp(class_name)
+      [self, superclass].each do |resource_holder|
+        if resource_holder.const_defined? class_name, false
+          resource_holder.send(:remove_const, class_name)
+        end
+      end
+    end
+  end
+
   class Provider
     # Chef provider for a resource
     class LWRPBase < Provider
-      class << self
-        alias_method :old_build_from_file, :build_from_file
-
-        #
-        # Override Opscode provider to remove any existing LWRPs to suppress
-        # constant re-definition warnings.
-        #
-        # @param [String] cookbook_name
-        #   the name of the cookbook
-        # @param [String] filename
-        #   file to load as a LWRP
-        # @param [Chef::RunContext] run_context
-        #   context of a Chef Run
-        #
-        # @return [Chef::Provider]
-        #
-        def build_from_file(cookbook_name, filename, run_context)
-          provider_name = filename_to_qualified_string(cookbook_name, filename)
-          class_name    = convert_to_class_name(provider_name)
-
-          remove_existing_lwrp(class_name)
-          old_build_from_file(cookbook_name, filename, run_context)
-        end
-
-        #
-        # Remove any existing Chef provider or resource with the specified name.
-        #
-        # @param [String] class_name
-        #   The class name. Must be a valid constant name.
-        #
-        def remove_existing_lwrp(class_name)
-          [self, superclass].each do |resource_holder|
-            if resource_holder.const_defined? class_name, false
-              resource_holder.send(:remove_const, class_name)
-            end
-          end
-        end
-      end
+      extend RemoveExistingLWRP
 
       module InlineResources
         module ClassMethods
@@ -64,44 +71,7 @@ class Chef
   class Resource
     # Chef provider for a resource
     class LWRPBase < Resource
-      class << self
-        alias_method :old_build_from_file, :build_from_file
-
-        #
-        # Override Opscode provider to remove any existing LWRPs to suppress
-        # constant re-definition warnings.
-        #
-        # @param [String] cookbook_name
-        #   the name of the cookbook
-        # @param [String] filename
-        #   file to load as a LWRP
-        # @param [Chef::RunContext] run_context
-        #   context of a Chef Run
-        #
-        # @return [Chef::Provider]
-        #
-        def build_from_file(cookbook_name, filename, run_context)
-          provider_name = filename_to_qualified_string(cookbook_name, filename)
-          class_name    = convert_to_class_name(provider_name)
-
-          remove_existing_lwrp(class_name)
-          old_build_from_file(cookbook_name, filename, run_context)
-        end
-
-        #
-        # Remove any existing Chef provider or resource with the specified name.
-        #
-        # @param [String] class_name
-        #   The class name. Must be a valid constant name.
-        #
-        def remove_existing_lwrp(class_name)
-          [self, superclass].each do |resource_holder|
-            if resource_holder.const_defined? class_name, false
-              resource_holder.send(:remove_const, class_name)
-            end
-          end
-        end
-      end
+      extend RemoveExistingLWRP
     end
   end
 end
