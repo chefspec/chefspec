a39e3b122f69f7a95efc7eb834de33932699bef4
Nathan Zook <nzook@whitepages.com>
2013-11-11 18:49:04 -0800

    de-meta specs

--- spec/unit/extensions/lwrp_base_spec.rb
+++ spec/unit/extensions/lwrp_base_spec.rb
@@ -6,40 +6,58 @@ if Chef::VERSION >= '11.0.0'
   module ChefSpec
     module Extensions
       describe :LWRPBase do
-
         describe '#remove_existing_lwrp' do
-          def run_check(container, remover)
-            const_name = :MysqlDatabase
-            sense = container == remover ? :to_not : :to
-            containing_class = Chef.const_get(container)::LWRPBase
-            removing_class = Chef.const_get(remover)::LWRPBase
-
-            containing_class.const_set(const_name, nil)
-            removing_class.remove_existing_lwrp(const_name.to_s)
-            expect(containing_class.constants).send(sense, include(const_name))
-            containing_class.send(:remove_const, const_name) if container != remover
+          before do
+            Chef::Provider::MysqlDatabase = nil
+            Chef::Resource::MysqlDatabase = nil
           end
 
-          it 'when called in Provider, removes the provider if it already exists' do
-            run_check(:Provider, :Provider)
+          after do
+            [Chef::Provider, Chef::Resource].each do |mod|
+              next unless mod.const_defined?(:MysqlDatabase, false)
+              mod.send(:remove_const, :MysqlDatabase)
+            end
           end
 
-          it 'when called in Provider, does not remove resource' do
-            run_check(:Resource, :Provider)
+          context Chef::Provider do
+            before do
+              Chef::Provider::LWRPBase.remove_existing_lwrp('MysqlDatabase')
+            end
+
+            it 'removes the provider if it already exists' do
+              expect(Chef::Provider.constants).to_not include(:MysqlDatabase)
+            end
+
+            it 'does not remove resource'  do
+              expect(Chef::Resource.constants).to include(:MysqlDatabase)
+            end
+
+            it 'does not throw an error if the resource does not already exist' do
+              expect {
+                Chef::Provider::LWRPBase.remove_existing_lwrp 'Human'
+              }.to_not raise_error
+            end
           end
 
-          it 'when called in Resource, removes the resource if it already exists' do
-            run_check(:Resource, :Resource)
-          end
+          context Chef::Resource do
+            before do
+              Chef::Resource::LWRPBase.remove_existing_lwrp('MysqlDatabase')
+            end
 
-          it 'when called in Resource, does not remove the provider' do
-            run_check(:Provider, :Resource)
-          end
+            it 'removes the resource if it already exists' do
+              expect(Chef::Resource.constants).to_not include(:MysqlDatabase)
+            end
 
-          it 'does not throw an error if the resource does not already exist' do
-            expect {
-              Chef::Provider::LWRPBase.remove_existing_lwrp 'Human'
-            }.to_not raise_error
+            it 'does not remove the provider'  do
+              Chef::Resource::LWRPBase.remove_existing_lwrp('MysqlDatabase')
+              expect(Chef::Provider.constants).to include(:MysqlDatabase)
+            end
+
+            it 'does not throw an error if the resource does not already exist' do
+              expect {
+                Chef::Resource::LWRPBase.remove_existing_lwrp 'Human'
+              }.to_not raise_error
+            end
           end
         end
       end
