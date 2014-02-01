7da94cb9eb5866d1d3498006bd69960e4eb4ee3a
Seth Vargo <sethvargo@gmail.com>
2014-01-31 21:14:26 -0500

    Add subscribes matchers

---
+++ lib/chefspec/api/subscriptions.rb
@@ -0,0 +1,35 @@
+module ChefSpec::API
+  # @since 3.2.0
+  module SubscriptionsMatchers
+    #
+    # Assert that a resource subscribes to another. Given a Chef Recipe that
+    # subscribes a template resource to restart apache:
+    #
+    #     service 'apache2' do
+    #       subscribes :create, 'template[/etc/apache2/config]'
+    #     end
+    #
+    # The Examples section demonstrates the different ways to test a
+    # subscription on a resource with ChefSpec.
+    #
+    # @example Assert a basic subscription
+    #   service = chef_run.service('apache2')
+    #   expect(service).to subscribe_to('template[/etc/apache2/config]')
+    #
+    # @example Assert a subscription with specified action
+    #   expect(service).to subscribe_to('template[/etc/apache2/config]').on(:restart)
+    #
+    # @example Assert a subscription with specified action and timing
+    #   expect(service).to subscribe_to('template[/etc/apache2/config]').on(:restart).immediately
+    #
+    #
+    # @param [String] signature
+    #   the signature of the notification to match
+    #
+    # @return [ChefSpec::Matchers::NotificationsMatcher]
+    #
+    def subscribe_to(signature)
+      ChefSpec::Matchers::SubscribesMatcher.new(signature)
+    end
+  end
+end
