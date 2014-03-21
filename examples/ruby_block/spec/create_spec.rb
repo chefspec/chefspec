4b76d4d4c7b911064ce1e63d4cada491635919b1
brandocorp <brandocorp@gmail.com>
2014-03-21 12:21:35 -0700

    fixing create spec for ruby_block
    the :create action is NOT the default action for ruby_block resources

--- examples/ruby_block/spec/create_spec.rb
+++ examples/ruby_block/spec/create_spec.rb
@@ -3,11 +3,6 @@ require 'chefspec'
 describe 'ruby_block::create' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  it 'creates a ruby_block with the default action' do
-    expect(chef_run).to create_ruby_block('default_action')
-    expect(chef_run).to_not create_ruby_block('not_default_action')
-  end
-
   it 'creates a ruby_block with an explicit action' do
     expect(chef_run).to create_ruby_block('explicit_action')
   end
