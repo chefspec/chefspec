4ac0e873ca799520df2db97aee0fa267a6aea162
Xabier de Zuazo <xabier@onddo.com>
2014-07-23 03:15:11 +0200

    Added ruby_block action :create

---
+++ examples/ruby_block/recipes/create.rb
@@ -0,0 +1,10 @@
+ruby_block 'default_action'
+
+ruby_block 'explicit_action' do
+  action :create
+end
+
+ruby_block 'specifying the identity attribute' do
+  block_name 'identity_attribute'
+  action :create
+end
---
+++ examples/ruby_block/spec/create_spec.rb
@@ -0,0 +1,19 @@
+require 'chefspec'
+
+describe 'ruby_block::create' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  it 'runs a ruby_block with the default action' do
+    expect(chef_run).to run_ruby_block('default_action')
+    expect(chef_run).to_not create_ruby_block('default_action')
+    expect(chef_run).to_not create_ruby_block('not_default_action')
+  end
+
+  it 'creates a ruby_block with an explicit action' do
+    expect(chef_run).to create_ruby_block('explicit_action')
+  end
+
+  it 'creates a ruby_block when specifying the identity attribute' do
+    expect(chef_run).to create_ruby_block('identity_attribute')
+  end
+end
