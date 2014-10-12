5c098627432f2d9737b8c78d6ceb9e2a30011417
Seth Vargo <sethvargo@gmail.com>
2014-10-12 18:38:29 -0400

    Deprecate ChefSpec::Runner methods
    ChefSpec::Runner.new has been replaced with either
    ChefSpec::SoloRunner.new or ChefSpec::ServerRunner.new. 
    ChefSpec::Runner.define_runner_method has been replaced by
    ChefSpec.define_matcher.
    
    The define_matcher helper does not dynamically define new methods on
    the runner object. Instead, matchers are put into a hash and then
    looked up by the ChefSpec::Runner implementation at runtime.

--- lib/chefspec.rb
+++ lib/chefspec.rb
@@ -1,5 +1,47 @@
 require 'rspec'
 
+module ChefSpec
+  #
+  # Defines a new runner method on the Chef runner.
+  #
+  # @param [Symbol] resource_name
+  #   the name of the resource to define a method
+  #
+  # @return [self]
+  #
+  def define_matcher(resource_name)
+    matchers[resource_name.to_sym] = Proc.new do |identity|
+      find_resource(resource_name, identity)
+    end
+
+    self
+  end
+  module_function :define_matcher
+
+  #
+  # The source root of the ChefSpec gem. This is useful when requiring files
+  # that are relative to the root of the project.
+  #
+  # @return [Pathname]
+  #
+  def root
+    @root ||= Pathname.new(File.expand_path('../../', __FILE__))
+  end
+  module_function :root
+
+  protected
+
+  #
+  # The list of custom defined matchers.
+  #
+  # @return [Hash<String, Proc>]
+  #
+  def matchers
+    @matchers ||= {}
+  end
+  module_function :matchers
+end
+
 require_relative 'chefspec/extensions/chef/client'
 require_relative 'chefspec/extensions/chef/conditional'
 require_relative 'chefspec/extensions/chef/data_query'
@@ -22,6 +64,7 @@ require_relative 'chefspec/stubs/search_registry'
 require_relative 'chefspec/stubs/search_stub'
 
 require_relative 'chefspec/api'
+require_relative 'chefspec/cacher'
 require_relative 'chefspec/coverage'
 require_relative 'chefspec/errors'
 require_relative 'chefspec/expect_exception'
@@ -30,20 +73,9 @@ require_relative 'chefspec/macros'
 require_relative 'chefspec/matchers'
 require_relative 'chefspec/renderer'
 require_relative 'chefspec/rspec'
-require_relative 'chefspec/runner'
+require_relative 'chefspec/server_runner'
+require_relative 'chefspec/solo_runner'
 require_relative 'chefspec/util'
 require_relative 'chefspec/version'
 
-module ChefSpec
-  extend self
-
-  #
-  # The source root of the ChefSpec gem. This is useful when requiring files
-  # that are relative to the root of the project.
-  #
-  # @return [Pathname]
-  #
-  def root
-    @root ||= Pathname.new(File.expand_path('../../', __FILE__))
-  end
-end
+require_relative 'chefspec/deprecations'
--- lib/chefspec/deprecations.rb
+++ lib/chefspec/deprecations.rb
@@ -15,128 +15,23 @@ module Kernel
 end
 
 module ChefSpec
-  # @deprecated {ChefSpec::ChefRunner} is deprecated. Please use
-  #   {ChefSpec::Runner} instead.
-  class ChefRunner
-    def self.new(*args, &block)
-      deprecated '`ChefSpec::ChefRunner` is deprecated. Please use' \
-        ' `ChefSpec::Runner` instead.'
-
-      ChefSpec::Runner.new(*args, &block)
-    end
-  end
-end
-
-module ChefSpec
   class Runner
-    alias_method :existing_initialize, :initialize
-    def initialize(*args, &block)
-      if args.first.is_a?(Hash)
-        if args.first.has_key?(:evaluate_guards)
-          deprecated 'The `:evaluate_guards` option is deprecated. Guards are' \
-            ' automatically evaluated by default. Please use `stub_command` to' \
-            ' stub shell guards.'
-        end
+    # @deprecated {ChefSpec.define_runner_method} is deprecated. Please
+    #   use {ChefSpec.define_runner_method} instead.
+    def self.define_runner_method(resource_name)
+      deprecated "`ChefSpec.define_runner_method' is deprecated. " \
+        "Please use `ChefSpec.define_runner_method' instead."
 
-        if args.first.has_key?(:actually_run_shell_guards)
-          deprecated 'The `:actually_run_shell_guards` option is deprecated.' \
-            ' Shell commands must be stubbed using `stub_command`.'
-        end
-      end
-
-      existing_initialize(*args, &block)
-    end
-  end
-end
-
-module ChefSpec::API
-  #
-  # @todo Remove in v4.0.0
-  #
-  module DeprecatedMatchers
-    def be_owned_by(user, group)
-      deprecated "The `be_owned_by` matcher is deprecated. Please use:" \
-        "\n\n" \
-        "  expect(resource.owner).to eq('#{user}')\n" \
-        "  expect(resource.group).to eq('#{group}')" \
-        "\n\n" \
-        "instead"
-      raise ChefSpec::NoConversionError.new('be_owned_by')
+      ChefSpec.define_matcher(resource_name)
     end
 
-    def create_file_with_content(path, content)
-      deprecated "The `create_file_with_content` matcher is deprecated." \
-        " Please use `render_file(#{path.inspect})" \
-        ".with_content(#{content.inspect})` instead."
-      ChefSpec::Matchers::RenderFileMatcher.new(path).with_content(content)
-    end
+    # @deprecated {ChefSpec::Runner.new} is deprecated. Please use
+    #   {ChefSpec::SoloRunner} or {ChefSpec::ServerRunner} instead.
+    def self.new(*args, &block)
+      deprecated "`ChefSpec::Runner' is deprecated. Please use" \
+        " `ChefSpec::SoloRunner' or `ChefSpec::ServerRunner' instead."
 
-    [:package, :yum_package, :gem_package, :chef_gem].each do |type|
-      matcher_name = "install_#{type}_at_version".to_sym
-      define_method(matcher_name) do |name, version|
-        deprecated "The `#{matcher_name}` matcher is deprecated." \
-          " Please use `install_package(#{name.inspect})" \
-          ".with(version: #{version.inspect})` instead."
-        ChefSpec::Matchers::ResourceMatcher.new(type, :install, package)
-          .with(version: version)
-      end
-    end
-
-    [:bash, :csh, :perl, :python, :ruby, :script].each do |type|
-      matcher_name = "execute_#{type}_script".to_sym
-      define_method(matcher_name) do |name|
-        deprecated "The `#{matcher_name}` matcher is deprecated." \
-          " Please use `run_#{type}(#{name.inspect})` instead."
-        ChefSpec::Matchers::ResourceMatcher.new(type, :run, name)
-      end
-    end
-
-    def log(message)
-      deprecated "The `log` matcher is deprcated. Please use" \
-        " `write_log(#{message.inspect}) instead."
-      ChefSpec::Matchers::ResourceMatcher.new(:log, :write, message)
-    end
-
-    def set_service_to_start_on_boot(service)
-      deprecated "The `set_service_to_start_on_boot` matcher is" \
-        " deprecated. Please use `enable_service(#{service.inspect})`" \
-        " instead."
-      ChefSpec::Matchers::ResourceMatcher.new(:service, :enable, service)
-    end
-
-    def set_service_to_not_start_on_boot(service)
-      deprecated "The `set_service_to_not_start_on_boot` matcher is" \
-        " deprecated. Please use `enable_service(#{service.inspect})`" \
-        " with a negating argument instead."
-      raise ChefSpec::NoConversionError.new('set_service_to_start_on_boot')
-    end
-
-    def execute_ruby_block(name)
-      deprecated "The `execute_ruby_block` matcher is deprecated. Please" \
-        " use `run_ruby_block(#{name.inspect})` instead."
-      ChefSpec::Matchers::ResourceMatcher.new(:ruby_block, :run, name)
-    end
-
-    def execute_command(command)
-      deprecated "The `execute_command` matcher is deprecated. Please" \
-        " use `run_execute(#{command.inspect})` instead."
-      ChefSpec::Matchers::ResourceMatcher.new(:execute, :run, command)
-    end
-  end
-end
-
-module ChefSpec::API
-  module NotificationsMatchers
-    alias_method :new_notify, :notify
-    def notify(resource, action = nil)
-      if action
-        deprecated "The `notify` matcher arity has changed. Please use" \
-          " `notify(#{resource.inspect}).to(#{action.to_sym.inspect})`" \
-          " instead."
-          new_notify(resource).to(action.to_sym)
-      else
-        new_notify(resource)
-      end
+      ChefSpec::SoloRuner.new(*args, &block)
     end
   end
 end
@@ -144,11 +39,9 @@ end
 module ChefSpec::Error
   class NoConversionError < ChefSpecError
     def initialize(matcher)
-      message = "I cannot convert `#{matcher}` to use a new matcher format!" \
+      super "I cannot convert `#{matcher}` to use a new matcher format!" \
         " Please see the ChefSpec documentation and CHANGELOG for details" \
         " on converting this matcher. Sorry :("
-
-      super(message)
     end
   end
 end
