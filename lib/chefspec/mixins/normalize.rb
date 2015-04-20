5b9b64184f105b38801897cf6d79ff8a115c44f0
Torben Knerr <mail@tknerr.de>
2015-04-20 02:35:55 +0200

    use Chef::Resource#declared_type first if available (Chef >= 12), otherwise fall back to Chef::Resource#resource_name (Chef <= 11)

--- lib/chefspec/mixins/normalize.rb
+++ lib/chefspec/mixins/normalize.rb
@@ -9,7 +9,13 @@ module ChefSpec
     # @return [Symbol]
     #
     def resource_name(thing)
-      name = thing.respond_to?(:resource_name) ? thing.resource_name : thing
+      if thing.respond_to?(:declared_type)
+        name = thing.declared_type
+      elsif thing.respond_to?(:resource_name)
+        name = thing.resource_name
+      else
+        name = thing
+      end
       name.to_s.gsub('-', '_').to_sym
     end
   end
