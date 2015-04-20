9d1e75244623fa66fd1c43b59867cf6d8b64b6cc
Seth Vargo <sethvargo@gmail.com>
2015-04-19 22:03:16 -0400

    Supress chef_gem compile_time warnings

--- examples/chef_gem/recipes/install.rb
+++ examples/chef_gem/recipes/install.rb
@@ -1,13 +1,18 @@
-chef_gem 'default_action'
+chef_gem 'default_action' do
+  compile_time false if respond_to?(:compile_time)
+end
 
 chef_gem 'explicit_action' do
-  action :install
+  action       :install
+  compile_time false if respond_to?(:compile_time)
 end
 
 chef_gem 'with_attributes' do
-  version '1.0.0'
+  version      '1.0.0'
+  compile_time false if respond_to?(:compile_time)
 end
 
 chef_gem 'specifying the identity attribute' do
   package_name 'identity_attribute'
+  compile_time false if respond_to?(:compile_time)
 end
--- examples/chef_gem/recipes/upgrade.rb
+++ examples/chef_gem/recipes/upgrade.rb
@@ -1,13 +1,16 @@
 chef_gem 'explicit_action' do
-  action :upgrade
+  action       :upgrade
+  compile_time false if respond_to?(:compile_time)
 end
 
 chef_gem 'with_attributes' do
-  version '1.0.0'
-  action  :upgrade
+  version      '1.0.0'
+  action       :upgrade
+  compile_time false if respond_to?(:compile_time)
 end
 
 chef_gem 'specifying the identity attribute' do
   package_name 'identity_attribute'
   action       :upgrade
+  compile_time false if respond_to?(:compile_time)
 end
