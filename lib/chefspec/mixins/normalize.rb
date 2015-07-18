d491c2a569627744bd3a57b86030a4602621014b
Noah Kantrowitz <noah@coderanger.net>
2015-07-17 17:05:30 -0700

    Only use declared_type is not nil.
    This can end up happening if you are doing weird stuff with manually creating resource objects. Should be safe to ignore if unset.

--- lib/chefspec/mixins/normalize.rb
+++ lib/chefspec/mixins/normalize.rb
@@ -9,7 +9,7 @@ module ChefSpec
     # @return [Symbol]
     #
     def resource_name(thing)
-      if thing.respond_to?(:declared_type)
+      if thing.respond_to?(:declared_type) && thing.declared_type
         name = thing.declared_type
       elsif thing.respond_to?(:resource_name)
         name = thing.resource_name
