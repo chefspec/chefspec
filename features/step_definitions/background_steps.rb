284b1fcd679cf44595fe1c5ad3bafaba255e6119
Seth Vargo <sethvargo@gmail.com>
2015-07-26 16:56:02 -0400

    Clarify what is being copied

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -1,4 +1,7 @@
 Given /^I am using the "(.+)" cookbook$/ do |cookbook|
-  FileUtils.cp_r(File.join('examples', cookbook), expand_path('.'))
+  source      = File.join('examples', cookbook)
+  destination = expand_path('.')
+
+  FileUtils.cp_r(source, destination)
   cd(cookbook)
 end
