cd5b133dadc25dcca0485d3f93ee1f90c8a2ba07
Tom Duffield <tom@chef.io>
2017-02-10 16:27:30 -0600

    Cleanup server to avoid thread leakage during tests
    Signed-off-by: Tom Duffield <tom@chef.io>

--- lib/chefspec/server_methods.rb
+++ lib/chefspec/server_methods.rb
@@ -10,7 +10,7 @@ module ChefSpec
     # @return [ChefZero::Server]
     #
     def server
-      @server ||= ChefZero::Server.new(
+      @@server ||= ChefZero::Server.new(
         # Set the log level from RSpec, defaulting to warn
         log_level:  RSpec.configuration.log_level || :warn,
 
@@ -160,7 +160,7 @@ module ChefSpec
     #   to the server
     #
     def load_data(name, key, data = {})
-      @server.load_data({ key => { name => data } })
+      @@server.load_data({ key => { name => data } })
     end
 
     #
@@ -170,9 +170,9 @@ module ChefSpec
       args.unshift('organizations', 'chef')
 
       if args.size == 3
-        @server.data_store.list(args)
+        @@server.data_store.list(args)
       else
-        @server.data_store.get(args)
+        @@server.data_store.get(args)
       end
     end
 
@@ -189,7 +189,6 @@ module ChefSpec
               when :on_disk
                 require "tmpdir"
                 require "chef_zero/data_store/raw_file_store"
-                tmpdir = Dir.mktmpdir
                 ChefZero::DataStore::RawFileStore.new(Dir.mktmpdir)
               else
                 raise ArgumentError, ":#{option} is not a valid server_runner_data_store option. Please use either :in_memory or :on_disk."
