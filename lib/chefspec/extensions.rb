5ae5690713457f9050fcbae3578d8483c794796a
Lamont Granquist <lamont@scriptkiddie.org>
2017-04-19 14:54:08 -0700

    automatic matcher generation
    via hooks in the Chef::Resource this time
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

---
+++ lib/chefspec/extensions.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+module ChefSpec::Extensions
+  module Chef
+  end
+end
+
+# STOP! DO NOT ALPHABETIZE!
+require_relative 'extensions/chef/resource'  # must come before client extensions or anything that winds up loading resources
+require_relative 'extensions/chef/securable'
+require_relative 'extensions/chef/client'
+require_relative 'extensions/chef/conditional'
+require_relative 'extensions/chef/cookbook_uploader'
+require_relative 'extensions/chef/data_query'
+require_relative 'extensions/chef/lwrp_base'
+require_relative 'extensions/chef/resource/freebsd_package'
