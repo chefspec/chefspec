c0d136e258fdb044e3e3e4aad50db0c74b185aa6
Tim Smith <tsmith@chef.io>
2018-01-23 13:10:21 -0800

    Use gentoo 4.9.6 definition
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- examples/paludis_package/spec/remove_spec.rb
+++ examples/paludis_package/spec/remove_spec.rb
@@ -1,7 +1,7 @@
 require 'chefspec'
 
 describe 'paludis_package::remove' do
-  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'gentoo', version: '2.2').converge(described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'gentoo', version: '4.9.6-gentoo-r1').converge(described_recipe) }
 
   it 'removes a paludis_package with an explicit action' do
     expect(chef_run).to remove_paludis_package('explicit_action')
