e9546dd3e9044f4c5f3b9a2b611204caa03211c9
Noah Kantrowitz <noah@coderanger.net>
2018-05-16 22:47:05 -0700

    Rework the nested collection hack so it can be run-time scoped rather than a compile-time thing.
    Not fixing Chef 12 mostly because I don't care, but I think the same patch should work there too (on >=12.5).

--- lib/chefspec/extensions/chef/lwrp_base.rb
+++ lib/chefspec/extensions/chef/lwrp_base.rb
@@ -14,13 +14,15 @@ if defined?(Chef::Provider::InlineResources)
 
   Chef::Provider::InlineResources::ClassMethods.prepend(Module.new do
     def action(name, &block)
+      # Note: This does not check $CHEFSPEC_MODE.
       define_method("action_#{name}", &block)
     end
   end)
 else # >= 13.0
-  Chef::Provider.singleton_class.prepend(Module.new do
-    def action(name, &block)
-      define_method("action_#{name}", &block)
+  Chef::Provider.prepend(Module.new do
+    def compile_and_converge_action(&block)
+      return super unless $CHEFSPEC_MODE
+      instance_eval(&block)
     end
   end)
 end
