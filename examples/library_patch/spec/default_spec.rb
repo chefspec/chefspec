70cce6489ac66b2b497163143f239f3eb74b4816
Noah Kantrowitz <noah@coderanger.net>
2018-05-25 14:31:31 -0500

    Tests for monkey patching cookbook libraries.

---
+++ examples/library_patch/spec/default_spec.rb
@@ -0,0 +1,14 @@
+describe 'library_patch' do
+  platform 'ubuntu', '16.04'
+
+  context 'with no patch' do
+    it { is_expected.to write_log('Hello 1') }
+  end
+
+  context 'with a patch' do
+    before do
+      expect(LibraryPatchHelpers).to receive(:helper_method).and_return(2)
+    end
+    it { is_expected.to write_log('Hello 2') }
+  end
+end
