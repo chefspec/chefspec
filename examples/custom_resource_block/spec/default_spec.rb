d95581070bf0e84f852f4ac1d5a9847ddd9fa3ad
Noah Kantrowitz <noah@coderanger.net>
2018-05-28 03:23:25 -0700

    Make this an actual test.

--- examples/custom_resource_block/spec/default_spec.rb
+++ examples/custom_resource_block/spec/default_spec.rb
@@ -1,8 +1,10 @@
 describe 'custom_resource_block' do
   platform 'ubuntu', '16.04'
+  step_into :custom_resource_block
   recipe do
     custom_resource_block 'name'
   end
 
-  it { subject }
+  it { is_expected.to run_custom_resource_block('name') }
+  it { is_expected.to write_log('hello world') }
 end
