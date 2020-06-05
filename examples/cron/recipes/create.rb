e26c83d44d3d78edb70527387d22c2160e2693ba
antima-gupta <agupta@msystechnologies.com>
2020-06-05 13:28:00 +0530

    Added command property in the cron recipes.
    Signed-off-by: antima-gupta <agupta@msystechnologies.com>

--- examples/cron/recipes/create.rb
+++ examples/cron/recipes/create.rb
@@ -1,10 +1,14 @@
-cron 'default_action'
+cron 'default_action' do
+  command "/bin/true"
+end
 
 cron 'explicit_action' do
+  command "/bin/true"
   action :create
 end
 
 cron 'with_attributes' do
   minute '0'
   hour   '20'
+  command "/bin/true"
 end
