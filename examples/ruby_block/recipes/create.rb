d70a5e34981878dac3953d48c4ac24549c03553a
Tim Smith <tsmith84@gmail.com>
2021-02-12 20:21:09 -0800

    Remove specs for deprecated actions
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- examples/ruby_block/recipes/create.rb
+++
@@ -1,8 +0,0 @@
-ruby_block 'explicit_action' do
-  action :run
-end
-
-ruby_block 'specifying the identity attribute' do
-  block_name 'identity_attribute'
-  action :run
-end
--- examples/yum_repository/spec/add_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'chefspec'
-
-describe 'yum_repository::add' do
-  platform 'centos'
-
-  describe 'creates a yum_repository with add action' do
-    it { is_expected.to add_yum_repository('explicit_add_action') }
-  end
-end
